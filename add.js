cef559e32cb487509b3187497d792c077175bdd4
Santhosh Kumar S <santhosh10287@gmail.com>
2022-09-28 14:29:53 +0530

    addition logic modified

--- add.js
+++ add.js
@@ -3,7 +3,7 @@ function add() {
 var num1 =document.getElementById("Firstno").value
 var num2=document.getElementById("Secondno").value
 
-var result =num1+num2
+var result =pareseInt(num1)+parseInt(num2)
 
 document.getElementById("result").value=result
 }
