ef3e002558483a260d607c6b80e6d135522cf1e3
Santhosh Kumar S <santhosh10287@gmail.com>
2022-09-28 14:21:43 +0530

    logic Created

---
+++ add.js
@@ -0,0 +1,9 @@
+function add() {
+
+var num1 =document.getElementById("Firstno").value
+var num2=document.getElementById("Secondno").value
+
+var result =num1+num2
+
+document.getElementById("result").value=result
+}
